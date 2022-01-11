dbdcfb68dfdbe09787fe89012b470ea814d74160
Avihu Harush <avihu@isrtech.co.il>
2022-01-12 00:29:59 +0300

    allow ios to record while playback is playing in the background

--- ios/Plugin/CustomMediaRecorder.swift
+++ ios/Plugin/CustomMediaRecorder.swift
@@ -13,6 +13,7 @@ class CustomMediaRecorder {
     private var recordingSession: AVAudioSession!
     private var audioRecorder: AVAudioRecorder!
     private var audioFilePath: URL!
+    private var originalRecordingSessionCategory: AVAudioSession.Category!
     private var status = CurrentRecordingStatus.NONE
     
     private let settings = [
@@ -29,7 +30,8 @@ class CustomMediaRecorder {
     public func startRecording() -> Bool {
         do {
             recordingSession = AVAudioSession.sharedInstance()
-            try recordingSession.setCategory(AVAudioSession.Category.record)
+            originalRecordingSessionCategory = recordingSession.category
+            try recordingSession.setCategory(AVAudioSession.Category.playAndRecord)
             try recordingSession.setActive(true)
             audioFilePath = getDirectoryToSaveAudioFile().appendingPathComponent("\(UUID().uuidString).aac")
             audioRecorder = try AVAudioRecorder(url: audioFilePath, settings: settings)
@@ -45,6 +47,8 @@ class CustomMediaRecorder {
         do {
             audioRecorder.stop()
             try recordingSession.setActive(false)
+            try recordingSession.setCategory(originalRecordingSessionCategory)
+            originalRecordingSessionCategory = nil
             audioRecorder = nil
             recordingSession = nil
             status = CurrentRecordingStatus.NONE
